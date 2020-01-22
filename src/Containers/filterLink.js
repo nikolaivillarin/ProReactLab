11be2e02e190acbada692f0700aaadb5df277a76
Nikolai Villarin <nikolai.villarin@gmail.com>
2020-01-21 21:50:49 -0500

    Checkpoint

---
+++ src/Containers/filterLink.js
@@ -0,0 +1,26 @@
+import { connect } from 'react-redux'
+import { setVisibilityFilter } from '../Actions/actions'
+import Link from '../Components/link'
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        active: ownProps.filter === state.visibilityFilter
+    }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        onClick: () => {
+            dispatch(setVisibilityFilter(ownProps.filter))
+        }
+    }
+}
+
+const FilterLink = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Link)
+
+export default FilterLink
+
+// NIKO YOU LEFT OFF CREATING VisibleTodoList.js
